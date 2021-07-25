f2192bfff36fa9301741f7e2bf6ae8e6e6cf4c62
anujmore137 <moreanuj1307@gmail.com>
2021-07-25 10:14:38 +0530

    Java basic

---
+++ Java/basic java/p1/p1.java
@@ -0,0 +1,17 @@
+class p1{
+    public static void main(String args[]){
+
+        
+        for (int i=0;i<10;i++){
+            if (i%2==0){
+                System.out.println(i+"is even");
+            }
+            else{
+                System.out.println(i+"is odd");   
+            }
+            
+        }
+        System.out.print("s");
+
+    }
+}
---
+++ Java/basic java/p2_fibo/fibo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;;
+
+public class fibo {
+
+    // 0 1 1 2 3 5
+    public static void main(String args[]) {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of series ");
+        Integer num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.print(a + ",");
+            c = a + b;
+
+            a = b;
+            b = c;
+
+        }
+        sc.close();
+    }
+
+}
