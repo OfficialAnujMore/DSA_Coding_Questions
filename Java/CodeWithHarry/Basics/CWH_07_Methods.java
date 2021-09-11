8496bdbdfd5f5f1bf8347ea9677b25f11c2ede05
anujmore137 <moreanuj1307@gmail.com>
2021-09-11 16:27:34 +0530

    new folder added

---
+++ Java/CodeWithHarry/Basics/CWH_07_Methods.java
@@ -0,0 +1,10 @@
+class CWH_07_Methods {
+
+  private static int logic(int x, int y) {
+    return x + y;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(logic(2, 3));
+  }
+}
