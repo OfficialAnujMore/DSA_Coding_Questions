daf3ac376eec3ec4a5911e31f6aafc54b291fb97
Anuj C More <moreanuj1307@gmail.com>
2021-11-04 18:10:10 +0530

    Abstract class

--- Java/CodeWithHarry/Practice/CWH_07_Library.java
+++ Java/CodeWithHarry/Practice/CWH_07_Library.java
@@ -1,29 +1,27 @@
 import java.util.ArrayList;
-class Management{
 
-    // String[] warehouse = new String[] {"Book1","Book2","Book3","Book4","Book5","Book6"};
-    ArrayList <String> warehouse = new ArrayList<String>();
+class Management {
 
+  // String[] warehouse = new String[] {"Book1","Book2","Book3","Book4","Book5","Book6"};
+  ArrayList<String> warehouse = new ArrayList<String>();
 
-    ArrayList<String> cars = new ArrayList<String>();
+  ArrayList<String> cars = new ArrayList<String>();
 
+  public void addBook(String BookName) {
+    warehouse.add(BookName);
+  }
 
-    public void addBook(String BookName){
-        warehouse.add(BookName);
-    }
-
-    public  int displayBook(){
-return warehouse;
-    }
+  public int displayBook() {
+    return warehouse;
+  }
 }
 
 class CWH_07_Library {
 
   public static void main(String[] args) {
-
     Management mng = new Management();
 
-    mng.warehouse.add(new String[]{"Book1","Book2","Book"});
-    // System.out.println(mng.displayBook());
+    mng.warehouse.add(new String[] { "Book1", "Book2", "Book" });
+    System.out.println(mng.displayBook());
   }
 }
---
+++ Java/CodeWithHarry/Practice/CWH_09_AbstractClass.java
@@ -0,0 +1,39 @@
+interface BasicAnimal {
+  void eat();
+  void sleep();
+}
+
+class Monkey {
+
+  public void jump() {
+    System.out.println("Jumping");
+  }
+
+  public void bite() {
+    System.out.println("Bite");
+  }
+}
+
+class Human extends Monkey implements BasicAnimal {
+
+  public void eat() {
+    System.out.println("Eating");
+  }
+
+  public void sleep() {
+    System.out.println("Sleeping");
+  }
+}
+
+class CWH_09_AbstractClass {
+
+  public static void main(String[] args) {
+    Human hum = new Human();
+    hum.eat();
+    hum.bite();
+    hum.jump();
+    hum.sleep();
+    Monkey m1 = new Human();
+    // m1.sleep(); Cannot use sleep method
+  }
+}
