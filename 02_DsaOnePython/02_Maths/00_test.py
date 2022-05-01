175bf15a0ee8f9846e3a27bd9c21325c66f7667a
Anuj C More <moreanuj1307@gmail.com>
2022-05-01 17:45:51 +0530

    Bit mappping

--- 02_DsaOnePython/02_Maths/00_test.py
+++ 02_DsaOnePython/02_Maths/00_test.py
@@ -1,2 +1,23 @@
-n = 16
-print(n % 10)
+def Add(x, y):
+ 
+    # Iterate till there is no carry
+    while (y != 0):
+     
+        # carry now contains common
+        # set bits of x and y
+        carry = x & y
+ 
+        # Sum of bits of x and y where at
+        # least one of the bits is not set
+        x = x ^ y
+ 
+        # Carry is shifted by one so that  
+        # adding it to x gives the required sum
+        y = carry << 1
+    print(bin(x))
+    return x
+
+# print(Add(8,7))
+
+print(bin((15 & 14)))
+print((15 & 14))
