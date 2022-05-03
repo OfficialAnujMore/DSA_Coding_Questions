10d5e89a4074a28a8adb0bafe2df912e715ff8ea
Anuj C More <moreanuj1307@gmail.com>
2022-05-03 23:46:24 +0530

    Test.py

--- 02_DsaOnePython/02_Maths/00_test.py
+++ 02_DsaOnePython/02_Maths/00_test.py
@@ -1,23 +1,10 @@
-def Add(x, y):
- 
-    # Iterate till there is no carry
-    while (y != 0):
-     
-        # carry now contains common
-        # set bits of x and y
-        carry = x & y
- 
-        # Sum of bits of x and y where at
-        # least one of the bits is not set
-        x = x ^ y
- 
-        # Carry is shifted by one so that  
-        # adding it to x gives the required sum
-        y = carry << 1
-    print(bin(x))
-    return x
+def check(num, mul):
+    print(num, mul)
 
-# print(Add(8,7))
+    return mul % num == mul & num-1
 
-print(bin((15 & 14)))
-print((15 & 14))
+
+num = 15
+for i in range(1, 11):
+    print(check(num, num*i))
+
