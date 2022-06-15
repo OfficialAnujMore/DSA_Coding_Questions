3062e1f55735aed55ab1adbe6790fb547598724b
Anuj C More <moreanuj1307@gmail.com>
2022-06-15 08:45:48 +0530

    Common function added

---
+++ 05_GRE/03_basic.py
@@ -0,0 +1,31 @@
+from randomWordGenerator import genrateWord
+basic3 = [
+    "avid",
+    "robust",
+    "malady",
+    "discrement",
+    "flounder",
+    "jocular",
+    "snide",
+    "moot",
+    "quandary",
+    "apprehension",
+    "inclement",
+    "patronize",
+    "pithy",
+    "plodding",
+    "impede",
+    "meander",
+    "consumate",
+    "archaic",
+    "proponent",
+    "respite",
+    "endemic",
+    "guffaw",
+    "entice",
+    "morose",
+    "transitory",
+    "renge"
+]
+
+genrateWord(basic3)
--- 05_GRE/randomWordGenerator.py
+++ 05_GRE/randomWordGenerator.py
@@ -1,33 +1,11 @@
 import random
-basic3 = [
-    "avid",
-    "robust",
-    "malady",
-    "discrement",
-    "flounder",
-    "jocular",
-    "snide",
-    "moot",
-    "quandary",
-    "apprehension",
-    "inclement",
-    "patronize",
-    "pithy",
-    "plodding",
-    "impede",
-    "meander",
-    "consumate",
-    "archaic",
-    "proponent",
-    ""
-]
 
-shouldGenerate = True
-
-while shouldGenerate:
-    y = input("To exit press Y ")
-    if (y.lower() == "y"):
-        shouldGenerate = False
-    else:
-        randomNumber = random.randint(0, len(basic3)-1)
-        print(basic3[randomNumber])
+def genrateWord(wordArr):
+    shouldExit = True
+    while shouldExit:
+        y = input("--------------- Press y to exit ------------------ ")
+        if (y.lower() == "y"):
+            shouldExit = False
+        else:
+            randomNumber = random.randint(0, len(wordArr)-1)
+            print(wordArr[randomNumber])
