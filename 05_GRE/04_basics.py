5a17a12f9ae319864a157b039c1ec995c85cf6b0
Anuj C More <moreanuj1307@gmail.com>
2022-06-17 08:39:47 +0530

    Basic 4 added

---
+++ 05_GRE/04_basics.py
@@ -0,0 +1,22 @@
+from randomWordGenerator import genrateWord
+basic4 = [
+    "buck",
+    "tumult",
+    "inadvertent",
+    "jargon",
+    "fickle",
+    "debase",
+    "pertinent",
+    "perpetutate",
+    "dertimental",
+    "martial",
+    "augment",
+    "impeccable",
+    "assuage",
+    "urbane",
+    "irresolute",
+    "tawdry",
+    "assail"
+]
+
+genrateWord(basic4)
